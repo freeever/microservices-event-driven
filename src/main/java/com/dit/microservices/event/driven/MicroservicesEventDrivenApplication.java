bb13edb14732496cbf149db2b0b7bc7e276e6568
Daniel Xu <daniel.xu2@ontario.ca>
2022-09-30 19:28:38 -0400

    Init commit

---
+++ src/main/java/com/dit/microservices/event/driven/MicroservicesEventDrivenApplication.java
@@ -0,0 +1,13 @@
+package com.dit.microservices.event.driven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesEventDrivenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesEventDrivenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dit/microservices/event/driven/MicroservicesEventDrivenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dit.microservices.event.driven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesEventDrivenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
